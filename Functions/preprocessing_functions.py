4e711f050778d41fea1e9b3399af36a88d763c7f
Ali Hossam <77911315+Ali-Hossam@users.noreply.github.com>
2023-12-26 20:40:52 +0200

    updated preprocessing functions

--- Functions/preprocessing_functions.py
+++ Functions/preprocessing_functions.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray,rgb2hsv
+from skimage.filters import median, gaussian
+from skimage import morphology
 
 def histogram_equalization(img):
     # Load image using io.imread()
@@ -22,3 +26,40 @@ def histogram_equalization(img):
     
     return image_normalized
 
+def plot_images(img1, img2, label1, label2):
+    """Plots two images side by side."""
+    # Create a figure with two subplots
+    fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+
+    # Plot the first image on the first subplot
+    axes[0].imshow(img1, cmap='gray')
+    axes[0].set_title(label1)
+
+    # Plot the second image on the second subplot
+    axes[1].imshow(img2, cmap='gray')
+    axes[1].set_title(label2)
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
+
+def ROI(img, x_start, x_end, y_start, y_end):
+    cropped_img = img[y_start : y_end, x_start : x_end]
+    return cropped_img
+
+def smooth(image):
+    """Applies median filter to smooth image."""
+    gray_image = rgb2gray(image)
+    f_image = median(gray_image)
+    return f_image
+
+def gray_to_binary(img, threshold):
+    """converts a gray scaled image to binary."""
+    binary_img = np.where(img > threshold, 1, 0)
+    return binary_img
+
+
+def apply_errosion(img, element_size):
+    element = morphology.rectangle(element_size, element_size+5)
+    opened_img = morphology.binary_erosion(img, element)
+    return opened_img
